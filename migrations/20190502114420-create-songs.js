1cd54f1a0fd2ba54fbd1cb1eb0c6ab4257f8aa39
QooMA <48360389+QooMA@users.noreply.github.com>
2019-05-04 10:38:43 +0900

    [Fix] #9 herokuでsequelize migrationできるように修正

---
+++ migrations/20190502114420-create-songs.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('songs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      package_id: {
+        type: Sequelize.SMALLINT
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      bpm: {
+        type: Sequelize.SMALLINT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('songs');
+  }
+};
